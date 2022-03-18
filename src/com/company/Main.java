bbdd42c76acf5238632c5c11555dde240de9de15
ArtemChurin <artem.churin.1976@gmail.com>
2022-03-18 11:02:48 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bonusLevel = 20; // кратность начисления бонусов
+
+        int ticketPrice = 45000; // стоимость билета
+
+        System.out.println("Сумма начисленных вам бонусных миль составляет:");
+        System.out.println(ticketPrice / bonusLevel);
+        
+    }
+}
